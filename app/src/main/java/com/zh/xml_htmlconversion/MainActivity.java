589a8b5fac1973eb4c25a2bc0ef76d4ca86fe192
Zohaib Hussain <zowaib@gmail.com>
2017-04-09 21:37:13 -0400

    adding in dependancies

--- app/src/main/java/com/zh/xml_htmlconversion/MainActivity.java
+++ app/src/main/java/com/zh/xml_htmlconversion/MainActivity.java
@@ -3,28 +3,26 @@ package com.zh.xml_htmlconversion;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Build;
+import android.os.Bundle;
 import android.os.Environment;
 import android.support.annotation.NonNull;
 import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
-import android.util.Log;
 
 import java.io.BufferedInputStream;
 import java.io.File;
 import java.io.IOException;
 
-import javax.xml.transform.ErrorListener;
 import javax.xml.transform.Source;
-import javax.xml.transform.Transformer;
 import javax.xml.transform.TransformerConfigurationException;
 import javax.xml.transform.TransformerException;
-import javax.xml.transform.TransformerFactory;
-import javax.xml.transform.TransformerFactoryConfigurationError;
 import javax.xml.transform.URIResolver;
-import javax.xml.transform.sax.SAXTransformerFactory;
 import javax.xml.transform.stream.StreamResult;
 import javax.xml.transform.stream.StreamSource;
+
+import full.org.apache.xalan.processor.TransformerFactoryImpl;
+import full.org.apache.xalan.transformer.TransformerImpl;
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -34,7 +32,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
         askForExternalStoragePermission();
 
         xmlToHtml();
@@ -42,15 +39,10 @@ public class MainActivity extends AppCompatActivity {
 
     private void xmlToHtml() {
         try {
-
-            //new BufferedInputStream(context.getAssets().open("spl.xsl"));
             Source xmlSource = new StreamSource(new BufferedInputStream(getAssets().open("sample1.xml")));
             Source xsltSource = new StreamSource(new BufferedInputStream(getAssets().open("spl.xsl")));
 
-/*            xmlSource.setSystemId(Environment.getExternalStorageDirectory().getAbsolutePath());
-            xsltSource.setSystemId(Environment.getExternalStorageDirectory().getAbsolutePath());*/
-            TransformerFactory transFact = TransformerFactory.newInstance();
-            //transFact.setAttribute ( "generate-translate" , Boolean.TRUE ) ;
+            TransformerFactoryImpl transFact = new TransformerFactoryImpl();
             transFact.setURIResolver(new URIResolver() {
                 @Override
                 public Source resolve(String href, String base) throws TransformerException {
@@ -63,38 +55,20 @@ public class MainActivity extends AppCompatActivity {
                 }
             });
 
-
-/*            // transformer created by factory.newTransformer(...)
-            TransformerImpl xalanTransformer = (TransformerImpl)transformer;
-            StylesheetRoot sroot = new StylesheetRoot((ErrorListener)null);
-// creates an empty Vector to be returned from getExtensions()
-            sroot.getExtensionNamespacesManager();
-// make sure the workaround works in Marsmallow too
-            sroot.setSecureProcessing(false);
-// sets up an empty ExtensionTable, so functionAvailable works without NPE
-            xalanTransformer.setExtensionsTable(sroot);*/
-
-
-
-            Transformer trans = transFact.newTransformer(xsltSource);
-            File f = new File(Environment.getExternalStorageDirectory().getAbsolutePath()+"/mydata.html");
+            TransformerImpl trans = (TransformerImpl) transFact.newTransformer(xsltSource);
+            File f = new File(Environment.getExternalStorageDirectory().getAbsolutePath() + "/mydata.html");
 
             StreamResult result = new StreamResult(f);
             trans.transform(xmlSource, result);
 
         } catch (TransformerConfigurationException e) {
-            Log.d(TAG, "TransformerConfigurationException: ");
-            e.printStackTrace();
-        } catch (TransformerFactoryConfigurationError e) {
-            Log.d(TAG, "TransformerFactoryConfigurationError: ");
             e.printStackTrace();
         } catch (TransformerException e) {
-            Log.d(TAG, "TransformerException: "+ e);
             e.printStackTrace();
-        } catch (IOException e){
-            Log.d(TAG, "IOException: ");
-        }
+        } catch (IOException e) {
+        e.printStackTrace();
     }
+}
 
     private void askForExternalStoragePermission() {
         if (Build.VERSION.SDK_INT > Build.VERSION_CODES.LOLLIPOP_MR1) {
@@ -106,7 +80,7 @@ public class MainActivity extends AppCompatActivity {
                 requestPermissions(new String[]{Manifest.permission.WRITE_EXTERNAL_STORAGE},
                         1);
             }
-        }else{
+        } else {
             xmlToHtml();
         }
     }
